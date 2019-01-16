48e319ce16bff3a551e0a40b4b5c87643b9c1e28
Ben Foster <ben.foster@checkout.com>
2019-01-16 22:38:08 +0000

    Added payment actions example and removed source example since not yet supported by the .NET SDK

--- spec/code_samples/C#/payments@{id}/get.cs
+++ spec/code_samples/C#/payments@{id}/get.cs
@@ -1,6 +1,6 @@
 var api = CheckoutApi.Create("your secret key");
 var threeDsSessionId = "sid_y3oqhf46pyzuxjbcn2giaqnb44";
-var payment = await api.Payments.GetAsync(threeDsSessionId);
+GetPaymentResponse payment = await api.Payments.GetAsync(threeDsSessionId);
 
 if (payment.Approved)
 {            
