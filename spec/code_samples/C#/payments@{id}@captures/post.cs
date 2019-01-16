159139af1430cd5ac52cef8e64e116af8d0c63b7
kevin-reece-cko <44969011+kevin-reece-cko@users.noreply.github.com>
2019-01-16 21:45:30 +0000

    Update spec/code_samples/C#/payments@{id}@captures/post.cs
    Co-Authored-By: ben-foster-cko <ben.foster@checkout.com>

--- spec/code_samples/C#/payments@{id}@captures/post.cs
+++ spec/code_samples/C#/payments@{id}@captures/post.cs
@@ -2,7 +2,7 @@ var api = CheckoutApi.Create("your secret key");
 var paymentId = "pay_y3oqhf46pyzuxjbcn2giaqnb44";
 
 // Full capture
-await _api.Payments.CaptureAsync(paymentId);
+await api.Payments.CaptureAsync(paymentId);
 
 // Or partial capture
 var captureRequest = new CaptureRequest
